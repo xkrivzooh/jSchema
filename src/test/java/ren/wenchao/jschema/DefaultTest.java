a1906940efb4bd4607b82c770a7a283bd3639b8b
xkrivzooh <xkrivzooh@gmail.com>
2022-07-22 21:26:17 +0800

    add nullable and default

---
+++ src/test/java/ren/wenchao/jschema/DefaultTest.java
@@ -0,0 +1,28 @@
+package ren.wenchao.jschema;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DefaultTest {
+
+    private static class A {
+        @Default("1")
+        int foo;
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(TypeSchema.getSchema(A.class).toString(true));
+        Assert.assertEquals("{\n" +
+                "  \"type\" : \"record\",\n" +
+                "  \"name\" : \"A\",\n" +
+                "  \"namespace\" : \"ren.wenchao.jschema.DefaultTest\",\n" +
+                "  \"types\" : { },\n" +
+                "  \"fields\" : [ {\n" +
+                "    \"name\" : \"foo\",\n" +
+                "    \"type\" : \"int\",\n" +
+                "    \"default\" : 1\n" +
+                "  } ]\n" +
+                "}", TypeSchema.getSchema(A.class).toString(true));
+    }
+}
