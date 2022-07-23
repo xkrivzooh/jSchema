f88778fd4b471d33985300e5892cb26cfe1b3aff
xkrivzooh <xkrivzooh@gmail.com>
2022-07-23 18:29:46 +0800

    add test

--- src/test/java/ren/wenchao/jschema/ParserTest.java
+++ src/test/java/ren/wenchao/jschema/ParserTest.java
@@ -1,50 +1,115 @@
 package ren.wenchao.jschema;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ParserTest {
 
     @Test
     public void test1() {
-        //language=JSON
-        String json = "{\n" +
-                "  \"type\": \"record\",\n" +
-                "  \"name\": \"B\",\n" +
-                "  \"namespace\": \"com.xkrivzooh.schema.avro.Test\",\n" +
-                "  \"fields\": [\n" +
-                "    {\n" +
-                "      \"name\": \"a\",\n" +
-                "      \"type\": {\n" +
-                "        \"type\": \"record\",\n" +
-                "        \"name\": \"A\",\n" +
-                "        \"fields\": [\n" +
-                "          {\n" +
-                "            \"name\": \"aInt\",\n" +
-                "            \"type\": \"int\"\n" +
-                "          },\n" +
-                "          {\n" +
-                "            \"name\": \"aInteger\",\n" +
-                "            \"type\": \"int\"\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      }\n" +
+        String schemaString = "{\n" +
+                "  \"type\" : \"record\",\n" +
+                "  \"name\" : \"D\",\n" +
+                "  \"namespace\" : \"ren.wenchao.jschema.NestedClassSchemaTest\",\n" +
+                "  \"types\" : {\n" +
+                "    \"ren.wenchao.jschema.NestedClassSchemaTest.A\" : {\n" +
+                "      \"type\" : \"record\",\n" +
+                "      \"name\" : \"A\",\n" +
+                "      \"namespace\" : \"ren.wenchao.jschema.NestedClassSchemaTest\",\n" +
+                "      \"types\" : {\n" +
+                "        \"java.lang.Object\" : {\n" +
+                "          \"type\" : \"record\",\n" +
+                "          \"name\" : \"Object\",\n" +
+                "          \"namespace\" : \"java.lang\",\n" +
+                "          \"types\" : { },\n" +
+                "          \"fields\" : [ ]\n" +
+                "        },\n" +
+                "        \"ren.wenchao.jschema.NestedClassSchemaTest.Enum\" : {\n" +
+                "          \"type\" : \"enum\",\n" +
+                "          \"name\" : \"Enum\",\n" +
+                "          \"namespace\" : \"ren.wenchao.jschema.NestedClassSchemaTest\",\n" +
+                "          \"symbols\" : [ \"e1\", \"e2\" ]\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"fields\" : [ {\n" +
+                "        \"name\" : \"aObject\",\n" +
+                "        \"type\" : \"java.lang.Object\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"enumA\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.Enum\"\n" +
+                "      } ]\n" +
                 "    },\n" +
-                "    {\n" +
-                "      \"name\": \"aInt\",\n" +
-                "      \"type\": \"int\"\n" +
+                "    \"ren.wenchao.jschema.NestedClassSchemaTest.B\" : {\n" +
+                "      \"type\" : \"record\",\n" +
+                "      \"name\" : \"B\",\n" +
+                "      \"namespace\" : \"ren.wenchao.jschema.NestedClassSchemaTest\",\n" +
+                "      \"types\" : { },\n" +
+                "      \"fields\" : [ {\n" +
+                "        \"name\" : \"bObject\",\n" +
+                "        \"type\" : \"java.lang.Object\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"ba1\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.A\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"ba2\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.A\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"enumB\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.Enum\"\n" +
+                "      } ]\n" +
                 "    },\n" +
-                "    {\n" +
-                "      \"name\": \"aInteger\",\n" +
-                "      \"type\": \"int\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"aa\",\n" +
-                "      \"type\": \"A\"\n" +
+                "    \"ren.wenchao.jschema.NestedClassSchemaTest.C\" : {\n" +
+                "      \"type\" : \"record\",\n" +
+                "      \"name\" : \"C\",\n" +
+                "      \"namespace\" : \"ren.wenchao.jschema.NestedClassSchemaTest\",\n" +
+                "      \"types\" : { },\n" +
+                "      \"fields\" : [ {\n" +
+                "        \"name\" : \"a\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.A\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"b\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.B\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"cObject\",\n" +
+                "        \"type\" : \"java.lang.Object\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"enumC\",\n" +
+                "        \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.Enum\"\n" +
+                "      } ]\n" +
                 "    }\n" +
-                "  ]\n" +
-                "}\n";
+                "  },\n" +
+                "  \"fields\" : [ {\n" +
+                "    \"name\" : \"a\",\n" +
+                "    \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.A\"\n" +
+                "  }, {\n" +
+                "    \"name\" : \"b\",\n" +
+                "    \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.B\"\n" +
+                "  }, {\n" +
+                "    \"name\" : \"c\",\n" +
+                "    \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.C\"\n" +
+                "  }, {\n" +
+                "    \"name\" : \"dObject\",\n" +
+                "    \"type\" : \"java.lang.Object\"\n" +
+                "  }, {\n" +
+                "    \"name\" : \"enumD\",\n" +
+                "    \"type\" : \"ren.wenchao.jschema.NestedClassSchemaTest.Enum\"\n" +
+                "  } ]\n" +
+                "}";
 
-        TypeSchema typeSchema = new Parser().parse(json);
+        TypeSchema typeSchema = TypeSchemaParser.parse(schemaString);
+
+        Assert.assertEquals("D", typeSchema.getName());
+        Assert.assertEquals("ren.wenchao.jschema.NestedClassSchemaTest", typeSchema.getNamespace());
+        Assert.assertEquals("ren.wenchao.jschema.NestedClassSchemaTest.D", typeSchema.getFullName());
+        Assert.assertEquals(5, typeSchema.getFields().size());
+
+        Field enumD = typeSchema.getField("enumD");
+        Assert.assertNotNull(enumD);
+        TypeSchema enumDSchema = enumD.schema();
+        Assert.assertEquals("ren.wenchao.jschema.NestedClassSchemaTest.Enum", enumDSchema.getFullName());
+
+
+
         System.out.println("");
     }
 }
